66225c1081c62ab9844aab2e686a50aeaf300668
jinyoung03 <wjdwlsdud1979@gmail.com>
2025-10-14 13:56:21 +0900

    화면 출력을 함수로 만든다.

--- break_building.py
+++ break_building.py
@@ -1,10 +1,54 @@
 from pico2d import *
-import random
-#1080,1920 해상도을 하고 싶은데 노트북이 작아서 안보임 일단 /2로 줄여서 작업
-open_canvas(540,960)
-start_img = load_image('C:/3dp/break_building/10.resource/start_screen_main.png')
-start_img.draw_now(270/2,480/2)
 
-delay(5)
+#1080,1920 해상도을 하고 싶은데 노트북이 작아서 안보임 일단 /2로 줄여서 작업
+
+class Start:
+    def __init__(self):
+        self.start_img = load_image('C:/3dp/break_building/10.resource/start_screen_main.png')
+        self.x, self.y = 270, 480
+        self.active =True
+    def update(self):
+        pass
+    def draw(self):
+        if self.active:
+            self.start_img.draw(self.x, self.y)
+    def handle_event(self, event):
+        if event.type == SDL_MOUSEBUTTONDOWN:
+            self.active = False
+            return True
+        return False
+
+
+def reset_world():
+    global running
+    running = True
+
+    global world
+    world = []
+
+    global start
+    start = Start()
+    world.append(start)
+
+
+open_canvas(540,960)
+
+reset_world()
+
+def update_world():
+    for object in world:
+        object.update()
+    pass
+
+def render_world():
+    clear_canvas()
+    for object in world:
+        object.draw()
+    update_canvas()
+
+while running:
+    update_world()
+    render_world()
+    delay(0.05)
 
 close_canvas()
