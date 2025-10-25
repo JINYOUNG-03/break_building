6c21f3bc6a4fdf244ba311ad9ed18e46e8f23bf2
jinyoung03 <wjdwlsdud1979@gmail.com>
2025-10-25 17:21:10 +0900

    캐릭터 이미지가 작아 크게 출력

--- character.py
+++ character.py
@@ -3,11 +3,18 @@ from pico2d import *
 class Character:
     def __init__(self):
         self.char_img = [load_image(f'10.resource/Char1_1_idle_{i+1}.png') for i in range(4)]
-        self.x, self.y = 540 / 2, 120
+        self.x, self.y = 540 / 2, 140
         self.frame = 0
+        self.scale = 2.0
+
+    def set_scale(self, scale: float):
+        self.scale = max(0.1, float(scale))
 
     def update(self):
         self.frame = (self.frame + 1) % 4
 
     def draw(self):
-        self.char_img[self.frame].draw(self.x, self.y)
+        img = self.char_img[self.frame]
+        w= int(img.w*self.scale)
+        h= int(img.h*self.scale)
+        img.draw(self.x, self.y, w, h)
