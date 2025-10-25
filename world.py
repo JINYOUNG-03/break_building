f44158e7111ed3828910812283b83a86cf569057
jinyoung03 <wjdwlsdud1979@gmail.com>
2025-10-25 17:11:36 +0900

    모든 객체가 담겨있는 world 생성

---
+++ world.py
@@ -0,0 +1,62 @@
+from start import Start
+from menu import GameMenu
+from gamestart import GameStart
+from character import Character
+
+def reset_world():
+    global running, start, menu, gamestart, character, current_screen, world
+    running = True
+    start = Start()
+    menu = GameMenu()
+    gamestart = GameStart()
+    character = Character()
+    current_screen = start
+    world = [current_screen]
+from pico2d import *
+def update_world():
+    for obj in world:
+        obj.update()
+
+def render_world():
+    clear_canvas()
+    for obj in world:
+        obj.draw()
+    update_canvas()
+
+class Start:
+    def __init__(self):
+        self.start_img = load_image('10.resource/start_screen_main.png')
+        self.x, self.y = 540 / 2, 960 / 2
+        self.active = True
+
+    def update(self):
+        pass
+
+    def draw(self):
+        if self.active:
+            self.start_img.draw(self.x, self.y)
+
+    def handle_event(self, event):
+        if event.type == SDL_KEYDOWN:
+            self.active = False
+            return True
+        return False
+
+def handle_events():
+    global running, current_screen, world
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                running = False
+            elif current_screen == start and event.key == SDLK_m:
+                current_screen = menu
+                world = [current_screen]
+            elif current_screen == menu and event.key == SDLK_s:
+                current_screen = gamestart
+                world = [current_screen, character]
+            elif current_screen == gamestart and event.key == SDLK_r:
+                current_screen = start
+                world = [current_screen]
