d47c525b3f4bd2638343647b1e168e860e87df02
jinyoung03 <wjdwlsdud1979@gmail.com>
2025-10-25 18:55:31 +0900

    world문 대대적인 수정

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 import time
 from pico2d import *
-from world import reset_world, update_world, render_world, handle_events
-#해상도 540x960 설정
+import world
 
+# 해상도 540x960 설정
 open_canvas(540, 960)
-reset_world()
+world.reset_world()
 
 prev_time = time.time()
-running = True
 
-while running:
+while world.running:
     now = time.time()
     dt = now - prev_time
     prev_time = now
 
-    handle_events()
-    update_world(dt)
-    render_world()
+    world.handle_events()
+    world.update_world(dt)
+    world.render_world()
     delay(0.01)
 
 close_canvas()
