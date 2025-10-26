2b17a03168ffd4ae4192390359a918535d889796
jinyoung03 <wjdwlsdud1979@gmail.com>
2025-10-26 18:21:10 +0900

    검의 좌표를 캐릭터 위치를 기반으로 변경하게

--- weapon.py
+++ weapon.py
@@ -1,13 +1,17 @@
 from pico2d import *
+import shared_state
 #아니 한 번 휘두르면 다시 원 상태가 아니고 오른쪽 왼쪽 반복해야함 자동차 와이퍼 생각
 class Weapon:
-    def __init__(self):
-        self.weapon_img = load_image(f'10.resource/1. Basic.png')
-        self.x, self.y = 254,175
-        self.w, self.h = 24,100
-
-    def update(self,dt):
-        pass
+    def __init__(self, owner=None, offset=(30, 0), image_path='10.resource/1. Basic.png'):
+        self.weapon_img = load_image(image_path)
+        self.owner = owner
+        self.offset_x, self.offset_y = offset
+        self.w, self.h = 24, 100   # 고정 출력 크기 100x24
+        self.x, self.y = shared_state.x, shared_state.y
+    def update(self, dt):
+        # 항상 shared_state 값을 따라감 (owner가 있어도 owner는 shared_state를 업데이트함)
+        self.x = shared_state.x + self.offset_x
+        self.y = shared_state.y + self.offset_y
 
     def draw(self):
-        self.weapon_img.draw(self.x, self.y,self.w, self.h)
+        self.weapon_img.draw(self.x, self.y, self.w, self.h)
