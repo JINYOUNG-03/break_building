18bb03b13a36101fbff3f4f7ed5fb939083d8d42
jinyoung03 <wjdwlsdud1979@gmail.com>
2025-10-26 18:00:18 +0900

    검 출력 생성/근데 너무 큼 ..

---
+++ weapon.py
@@ -0,0 +1,13 @@
+from pico2d import *
+#아니 한 번 휘두르면 다시 원 상태가 아니고 오른쪽 왼쪽 반복해야함 자동차 와이퍼 생각
+class Weapon:
+    def __init__(self):
+        self.weapon_img = load_image(f'10.resource/1. Basic.png')
+        self.x, self.y = 254, 131
+        self.active = False
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.weapon_img.draw(self.x, self.y)
