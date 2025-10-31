a013587a172d0ec91fb71a1e9e0eb8017f5b4098
jinyoung03 <wjdwlsdud1979@gmail.com>
2025-10-31 11:40:38 +0900

    기본적인 프레임 작성

--- weapon.py
+++ weapon.py
@@ -8,6 +8,19 @@ class Weapon:
         self.offset_x, self.offset_y = offset
         self.w, self.h = 24, 100   # 고정 출력 크기 100x24
         self.x, self.y = shared_state.x, shared_state.y
+        self.frame= 0
+        self.anim_acc = 0.0
+        self.frame_time 1.0/anim_fps
+        self.active= False
+        self.total_frames = len(self.images)
+        self.elasped =0.0
+        self.duration = self.total_frmaes * self.frame_time
+    def start(self):
+        if not self.active:
+            self.active = True
+            self.frame = 0
+            self.anim_acc = 0.0
+            self.elapsed =0.0
     def update(self, dt):
         # 항상 shared_state 값을 따라감 (owner가 있어도 owner는 shared_state를 업데이트함)
         self.x = shared_state.x + self.offset_x
