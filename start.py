93b95211f1050225babc5167bb25340f58d2567b
jinyeong <wjdwlsdud1979@gmail.com>
2025-11-18 22:25:18 +0900

    시작 화면 클릭으로 변경

--- start.py
+++ start.py
@@ -5,6 +5,7 @@ class Start:
         self.start_img = load_image('10.resource/start_screen_main.png')
         self.x, self.y = 540 / 2, 960 / 2
         self.active = True
+        self.font = load_font('ENCR10B.TTF', 30)
 
     def update(self):
         pass
@@ -12,6 +13,8 @@ class Start:
     def draw(self):
         if self.active:
             self.start_img.draw(self.x, self.y)
+            # 클릭 가능 영역 표시 (전체 화면)
+            self.font.draw(270, 200, 'Click anywhere to start', (255, 255, 255))
 
     def handle_event(self, event):
         if event.type == SDL_KEYDOWN:
