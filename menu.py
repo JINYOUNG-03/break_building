a4000e14bd29c428336c0d8bbc0882f4bb5d06a0
jinyeong <wjdwlsdud1979@gmail.com>
2025-11-18 22:15:25 +0900

    버튼 클릭 받게 변경

--- menu.py
+++ menu.py
@@ -11,3 +11,24 @@ class GameMenu:
 
     def draw(self):
         self.menu_img.draw(self.x, self.y)
+
+    def check_button_click(self, mouse_x, mouse_y):
+        """
+        마우스 클릭이 버튼 영역 안에 있는지 확인
+        반환값: 'play', 'tutorial', 'quit' 또는 None
+
+        버튼 위치는 menu.png 이미지에 맞게 조정 필요
+        """
+        # Play 버튼 영역 (예시 좌표, 실제 이미지에 맞게 조정)
+        if 170 <= mouse_x <= 370 and 600 <= mouse_y <= 680:
+            return 'play'
+
+        # Tutorial 버튼 영역 (예시 좌표)
+        if 170 <= mouse_x <= 370 and 480 <= mouse_y <= 560:
+            return 'tutorial'
+
+        # Quit 버튼 영역 (예시 좌표)
+        if 170 <= mouse_x <= 370 and 360 <= mouse_y <= 440:
+            return 'quit'
+
+        return None
